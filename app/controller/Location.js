322c6e7642f3ebfcab2c903c36065b605b5c9527
Bhushan <bhushan@weboniselab.com>
2013-03-22 19:14:29 +0530

    Sencha DemoNavigation App

--- app/controller/Location.js
+++ app/controller/Location.js
@@ -14,11 +14,22 @@ Ext.define("DemoNavigation.controller.Location", {
 
     getLocation: function(button) {
         console.log('The panel was rendered');
+//        Ext.device.Geolocation.watchPosition({
+//            frequency: 3000, // Update every 3 seconds
+//            callback: function(position) {
+//                console.log('Position updated!', position.coords);
+//            },
+//            failure: function() {
+//                console.log('something went wrong!');
+//            }
+//        });
         Ext.device.Geolocation.getCurrentPosition({
             success: function(position) {
+//                console.log(JSON.stringify(position.coords));
                 Ext.Msg.alert(JSON.stringify(position.coords));
             },
             failure: function() {
+//                console.log('something went wrong!');
                 Ext.Msg.alert('something went wrong!');
             }
         });
